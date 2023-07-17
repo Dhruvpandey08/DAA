3e4897b2ff6e814c77a22bf9ad9a8e82ee6d3b16
Dhruv <Dhruvpandey928@gmail.com>
2023-07-17 08:41:38 +0530

    Updated

--- LineSegment_Intersect.cpp
+++ LineSegment_Intersect.cpp
@@ -6,7 +6,7 @@ class Line
 public:
     bool on_segment(int xi, int xj, int xk, int yi, int yj, int yk)
     {
-        if(min(xi,xj)<=xk<=max(xi,xj) && min(yi,yj)<=yk<=max(yi,yj))
+        if (min(xi, xj) <= xk && xk <= max(xi, xj) && min(yi, yj) <= yk && yk <= max(yi, yj))
         {
             return true;
         }
@@ -16,37 +16,47 @@ public:
         }
     }
 
-    int direction(int xi,int yi,int xj,int yj,int xk,int yk)
+    int direction(int xi, int yi, int xj, int yj, int xk, int yk)
     {
         return (xk - xi) * (yj - yi) - (xj - xi) * (yk - yi);
     }
 
-    bool intersect(int p1x,int p1y,int p2x,int p2y,int p3x,int p3y,int p4x,int p4y)
+    bool intersect(int p1x, int p1y, int p2x, int p2y, int p3x, int p3y, int p4x, int p4y, int& intersectX, int& intersectY)
     {
-        int d1,d2,d3,d4;
-        d1 = direction(p3x,p3y,p4x,p4y,p1x,p1y);
-        d2 = direction(p3x,p3y,p4x,p4y,p2x,p2y);
-        d3 = direction(p1x,p1y,p2x,p2y,p3x,p3y);
-        d4 = direction(p1x,p1y,p2x,p2y,p4x,p4y);
+        int d1, d2, d3, d4;
+        d1 = direction(p3x, p3y, p4x, p4y, p1x, p1y);
+        d2 = direction(p3x, p3y, p4x, p4y, p2x, p2y);
+        d3 = direction(p1x, p1y, p2x, p2y, p3x, p3y);
+        d4 = direction(p1x, p1y, p2x, p2y, p4x, p4y);
 
-        if(((d1>0 && d2<0) || (d1<0 && d2>0)) && (d3>0 && d4<0) || (d3<0 && d4>0))
+        if (((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0)) && (d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0))
         {
+            intersectX = ((p1x * d2 - p2x * d1) * (p4x - p3x) - (p3x * d4 - p4x * d3) * (p2x - p1x)) / ((d2 - d1) * (p4x - p3x) - (d4 - d3) * (p2x - p1x));
+            intersectY = ((p1y * d2 - p2y * d1) * (p4y - p3y) - (p3y * d4 - p4y * d3) * (p2y - p1y)) / ((d2 - d1) * (p4y - p3y) - (d4 - d3) * (p2y - p1y));
             return true;
         }
-        else if(d1==0 && on_segment(p3x,p3y,p4x,p4y,p1x,p1y))
+        else if (d1 == 0 && on_segment(p3x, p3y, p4x, p4y, p1x, p1y))
         {
+            intersectX = p1x;
+            intersectY = p1y;
             return true;
         }
-        else if(d2==0 && on_segment(p3x,p3y,p4x,p4y,p2x,p2y))
+        else if (d2 == 0 && on_segment(p3x, p3y, p4x, p4y, p2x, p2y))
         {
+            intersectX = p2x;
+            intersectY = p2y;
             return true;
         }
-        else if(d3==0 && on_segment(p1x,p1y,p2x,p2y,p3x,p3y))
+        else if (d3 == 0 && on_segment(p1x, p1y, p2x, p2y, p3x, p3y))
         {
+            intersectX = p3x;
+            intersectY = p3y;
             return true;
         }
-        else if(d4==0 && on_segment(p1x,p1y,p2x,p2y,p4x,p4y))
+        else if (d4 == 0 && on_segment(p1x, p1y, p2x, p2y, p4x, p4y))
         {
+            intersectX = p4x;
+            intersectY = p4y;
             return true;
         }
         else
@@ -67,14 +77,15 @@ int main()
     cin >> p3x >> p3y;
     int p4x, p4y;
     cin >> p4x >> p4y;
-    bool intersect = line1.intersect(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y);
-    if(intersect)
+    int intersectX, intersectY;
+    bool isIntersect = line1.intersect(p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y, intersectX, intersectY);
+    if (isIntersect)
     {
-        cout<<"yes"<<endl;
+        cout << "Lines intersect at (" << intersectX << ", " << intersectY << ")" << endl;
     }
     else
     {
-        cout<<"No"<<endl;
+        cout << "Lines do not intersect" << endl;
     }
     return 0;
 }
--- PairLineSegment_Intersect.cpp
+++ PairLineSegment_Intersect.cpp
@@ -19,44 +19,49 @@ public:
         return (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
     }
 
-    bool intersect(const Point& p1, const Point& q1, const Point& p2, const Point& q2) {
+    bool intersect(const Point& p1, const Point& q1, const Point& p2, const Point& q2, Point& intersectPoint) {
         int d1 = direction(p1, q1, p2);
         int d2 = direction(p1, q1, q2);
         int d3 = direction(p2, q2, p1);
         int d4 = direction(p2, q2, q1);
 
         if ((d1 > 0 && d2 < 0 || d1 < 0 && d2 > 0) && (d3 > 0 && d4 < 0 || d3 < 0 && d4 > 0)) {
+            double ua = static_cast<double>(direction(p1, p2, q1)) / direction(p1, q1, q2);
+            intersectPoint.x = p2.x + ua * (q2.x - p2.x);
+            intersectPoint.y = p2.y + ua * (q2.y - p2.y);
             return true;
         }
 
         if (d1 == 0 && on_segment(p1, p2, q1)) {
+            intersectPoint = p2;
             return true;
         }
         if (d2 == 0 && on_segment(p1, q2, q1)) {
+            intersectPoint = q2;
             return true;
         }
         if (d3 == 0 && on_segment(p2, p1, q2)) {
+            intersectPoint = p1;
             return true;
         }
         if (d4 == 0 && on_segment(p2, q1, q2)) {
+            intersectPoint = q1;
             return true;
         }
 
         return false;
     }
 
-    int pairs(const vector<Point>& points) {
-        int count = 0;
+    void printIntersectingPairs(const vector<Point>& points) {
         int n = points.size();
-
         for (int i = 0; i < n - 3; i += 2) {
             for (int j = i + 2; j < n - 1; j += 2) {
-                if (intersect(points[i], points[i + 1], points[j], points[j + 1])) {
-                    count++;
+                Point intersectPoint;
+                if (intersect(points[i], points[i + 1], points[j], points[j + 1], intersectPoint)) {
+                    cout << "Intersecting Points: (" << intersectPoint.x << ", " << intersectPoint.y << ") and (" << points[i].x << ", " << points[i].y << ")" << endl;
                 }
             }
         }
-        return count;
     }
 };
 
@@ -74,8 +79,7 @@ int main() {
         points.push_back({ x2, y2 });
     }
 
-    int ans = line1.pairs(points);
-    cout << ans << endl;
+    line1.printIntersectingPairs(points);
 
     return 0;
 }
